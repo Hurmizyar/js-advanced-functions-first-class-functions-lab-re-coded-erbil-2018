432cc399b35fbc07a8a3674b2a11aebb8d3461d7
Ahmed kamil jemil <akjemil@gmail.com>
2018-07-25 12:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function (driver) {
+   let newArray= [driver[0],driver[1]];
+   return newArray;
+  } 
+  const returnLastTwoDrivers=function(hama){
+    return hama.slice(2)
+    
+  }
+  const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+  
+ const fareMultiplier = createFareMultiplier(num){
+   let num=argument;
+   return argument*4
+ }
